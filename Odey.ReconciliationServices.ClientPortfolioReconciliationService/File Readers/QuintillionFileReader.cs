3804b1b5c8b30cbac08f3c34445b3bf08e656825
Geoff Poore <g.poore@odey.com>
2023-05-18 18:58:43 +0100

    e

--- Odey.ReconciliationServices.ClientPortfolioReconciliationService/File Readers/QuintillionFileReader.cs
+++ Odey.ReconciliationServices.ClientPortfolioReconciliationService/File Readers/QuintillionFileReader.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Odey.Framework.Keeley.Entities.Enums;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -13,7 +14,18 @@ namespace Odey.ReconciliationServices.ClientPortfolioReconciliationService
         {
         }
 
-        protected override string FundColumnName { get { return "Classmantraid"; } }
+        protected override string FundColumnName
+        {
+            get
+            {
+                if (FundId == (int)FundIds.ARFF)
+                {
+                    return "SubClassMantraId";
+                }
+
+                return "Classmantraid";
+            }
+        }
         protected override string AccountReferenceColumnName { get { return "Investormantraid"; } }
         protected override string QuantityColumnName { get { return "Units"; } }
         protected override string MarketValueColumnName { get { return "Totalmv"; } }       
